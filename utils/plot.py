7896f0d64a24a8a960b86051d11a8cefc4f4dcc7
FennexFox <technocracy90@gmail.com>
2025-01-31 17:13:25 +0900

    remove unused plot_prediction function from utils/plot.py

--- utils/plot.py
+++ utils/plot.py
@@ -24,35 +24,3 @@ def plot_history(history, loss_ylim=(0, 5), acc_ylim=(0, 1)):
     ax2.legend()
 
     plt.show()
-
-def plot_prediction(model, test, batch_size = 32):
-    test_batch = test.shuffle(1024).batch(batch_size)
-    
-    for image_batch, label_batch in test_batch.take(1):
-        images = image_batch
-        labels = label_batch
-        predictions = model.predict(image_batch)
-    
-    predictions = tf.argmax(predictions, axis=1)
-    row_num = batch_size // 8 + 1
-    dis = plt.figure(figsize=(15, 2 * (row_num)))
-    
-    correct_count = 0
-    for idx, (image, label, prdiction) in enumerate(zip(images, labels, predictions)):
-        ax = dis.add_subplot(row_num, 8, idx + 1)
-        image = (image + 1) / 2
-        ax.imshow(image)
-        
-        is_correct = label == prdiction
-        title = f'Label: {label},\nPrediction: {prdiction}'
-        
-        if not is_correct:
-            ax.set_title(title, fontdict={'fontsize': 8, 'color': 'red'})
-        else:
-            ax.set_title(title, fontdict={'fontsize': 8, 'color': 'green'})
-            correct_count += 1
-
-        plt.axis('off')
-    
-    plt.show()
-    print(f'Correct Count: {correct_count}/{batch_size}')
