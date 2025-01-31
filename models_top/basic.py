084624a7e32bd6fba2f394257ff38335a0ae41e7
FennexFox <technocracy90@gmail.com>
2025-01-31 10:53:56 +0900

    fix models_top

--- models_top/basic.py
+++ models_top/basic.py
@@ -1,26 +1,27 @@
-import tensorflow, keras
+import tensorflow, keras, math
 from keras import layers
 
 def simple1():
     model = keras.Sequential([
         layers.GlobalAveragePooling2D(),
+        layers.Dense(512, activation='relu'),
+        layers.Dropout(0.5),
         layers.Dense(256, activation='relu'),
-        layers.Dropout(0.5)
     ])
     
-    model.name = "basic_simple1"
+    model.name = "top_simple1"
     
     return model
 
 def simple2():
     model = keras.Sequential([
         layers.GlobalAveragePooling2D(),
-        layers.Dense(256, activation='relu'),
+        layers.Dense(1024, activation='relu'),
         layers.Dropout(0.5),
-        layers.Dense(256, activation='relu')
+        layers.Dense(512, activation='relu')
     ])
     
-    model.name = "basic_simple2"
+    model.name = "top_simple2"
     
     return model
 
@@ -33,7 +34,7 @@ def midsize1():
         layers.Dense(256, activation='relu')
     ])
     
-    model.name = "basic_midsize1"
+    model.name = "top_midsize1"
     
     return model
 
@@ -52,12 +53,14 @@ def midsize2():
         layers.Dense(256, activation='relu')
     ])
     
-    model.name = "basic_midsize1"
+    model.name = "top_midsize1"
     
     return model
 
-def midsize3():
-    inputs = keras.Input(shape=(7, 7, 1280))
+def midsize3(input_shape = (480, 480, 1280)):
+    conv_output_shape = math.ceil(15 / 480 * input_shape[0])
+    
+    inputs = keras.Input(shape=(conv_output_shape, conv_output_shape, input_shape[2]))
     
     x = layers.GlobalAveragePooling2D()(inputs)
     x = layers.Dense(1024, activation='relu')(x)
@@ -78,5 +81,5 @@ def midsize3():
     x = layers.Dense(256, activation='relu')(x)
     output = layers.BatchNormalization()(x)
     
-    model = keras.Model(inputs=inputs, outputs=output, name="basic_midsize3")
+    model = keras.Model(inputs=inputs, outputs=output, name="top_midsize3")
     return model
