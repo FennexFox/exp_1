8001b09464a5e658e9ea84510af936f389472152
FennexFox <technocracy90@gmail.com>
2025-01-31 11:11:03 +0900

    add datasets load function

--- utils/data.py
+++ utils/data.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import keras, pathlib
 
 def example_display(dataset_info, dataset, num_examples=16, is_raw = False):
     row_num = num_examples // 8 + 1
@@ -29,3 +30,26 @@ def get_batches(train_ds, val_ds, test_ds, batch_size = 64):
     test_ds = test_ds.batch(batch_size)
 
     return train_ds, val_ds, test_ds
+
+def load_datasets_from_directory(base_dir, image_size = (224, 224), batch_size = 64, val_split = 0.4):
+    if not pathlib.Path(f"{base_dir}").exists():
+        raise ValueError(f"Directory {base_dir} does not exist")
+        
+    dataset = keras.utils.image_dataset_from_directory(
+        pathlib.Path(f"{base_dir}"),
+        validation_split=val_split,
+        subset="both",
+        seed=123,
+        image_size=image_size,
+        batch_size=batch_size,
+        label_mode='categorical'
+    )
+    
+    train_dataset = dataset[0]
+
+    # Split validation dataset into validation and test sets
+    val_batches = len(dataset[1]) // 2
+    val_dataset = dataset[1].take(val_batches)
+    test_dataset = dataset[1].skip(val_batches)
+    
+    return train_dataset, val_dataset, test_dataset
