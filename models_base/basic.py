f19539b74e3356954b771d65de9ebc46b0eff10e
FennexFox <technocracy90@gmail.com>
2025-01-31 14:29:03 +0900

    add base_simple3

--- models_base/basic.py
+++ models_base/basic.py
@@ -30,5 +30,22 @@ def base_simple2(input_shape = (480, 480, 3)):
     model.name = "base_simple2"
     
     return model
+
+def base_simple3(input_shape = (480, 480, 3)):
     
+    input = keras.Input(shape=input_shape)
+    features = input
+    
+    for i in range(5):
+        features = layers.Conv2D(2 ** (i + 4), (3, 3), padding = 'same', activation='relu')(features)
+        features = layers.Conv2D(2 ** (i + 4), (3, 3), padding = 'same', activation='relu')(features)
+        features = layers.Conv2D(2 ** (i + 4), (3, 3), padding = 'same', activation='relu')(features)
+        features = layers.MaxPooling2D(pool_size=(2, 2))(features)
+        
+    output = features
+        
+    model = models.Model(inputs=input, outputs=output)
+    model.name = "base_simple2"
+    
+    return model
     
